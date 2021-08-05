302e202fa23338a6bfef2231719d284759f679c9
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2021-08-05 17:59:13 -0600

    Add support for tiered residents

--- app.js
+++ app.js
@@ -4,21 +4,70 @@ import { dollarsToCents } from './utils.js'
 import NumberFormat from 'react-number-format'
 
 import {
-  Label,
+  Badge,
+  Button,
+  Card,
+  CardFooter,
+  CardHeader,
+  Col,
   Container,
-  Navbar,
-  NavbarBrand,
   Form,
   FormGroup,
-  Col,
+  Input,
+  Label,
+  ListGroup,
+  ListGroupItem,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+  Navbar,
+  NavbarBrand,
   Row
 } from 'reactstrap'
 
 class App extends Component {
   state = {
     monthlyRent: 6000,
-    residentsCount: 20,
-    monthsCount: 6
+    tiers: [
+      {
+        monthlyRent: 200,
+        residentsCount: 5,
+        description:
+          'Each resident is guaranteed one weekend bed spots per month.'
+      },
+      {
+        monthlyRent: 300,
+        residentsCount: 15,
+        description:
+          'Each resident is guaranteed two weekend bed spots per month.'
+      },
+      {
+        monthlyRent: 400,
+        residentsCount: 5,
+        description:
+          'Each resident is guaranteed three weekend bed spots per month.'
+      }
+    ],
+    modal: false
+  }
+
+  toggleModal = () => {
+    this.setState({ modal: !this.state.modal })
+  }
+
+  addTier = tier => {
+    this.setState({ tiers: [...this.state.tiers, tier] })
+  }
+  updateTier = (index, tier) => {
+    const tiers = this.state.tiers
+    tiers[index] = tier
+    this.setState({ tiers })
+  }
+  removeTier = index => {
+    const tiers = this.state.tiers
+    tiers.splice(index, 1)
+    this.setState({ tiers })
   }
 
   render () {
@@ -28,83 +77,53 @@ class App extends Component {
           <NavbarBrand className='mx-auto'>Ski Haus Calculator</NavbarBrand>
         </Navbar>
         <Container>
-          <Form>
-            <FormGroup>
-              <Label htmlFor='monthlyRent'>Monthly Rent</Label>
-              <NumberFormat
-                name='monthlyRent'
-                value={this.state.monthlyRent}
-                onValueChange={({ value }) =>
-                  this.setState({ monthlyRent: value })
-                }
-                thousandSeparator={true}
-                prefix={'$'}
-                allowNegative={false}
-                className='form-control'
-              />
-            </FormGroup>
-
-            <FormGroup>
-              <Label htmlFor='residentsCount'>Number of Residents</Label>
-              <NumberFormat
-                name='residentsCount'
-                value={this.state.residentsCount}
-                onValueChange={({ value }) =>
-                  this.setState({ residentsCount: value })
-                }
-                allowNegative={false}
-                className='form-control'
-              />
-            </FormGroup>
-
-            <FormGroup>
-              <Label htmlFor='monthsCount'>Number of Months</Label>
-              <NumberFormat
-                name='monthsCount'
-                value={this.state.monthsCount}
-                onValueChange={({ value }) =>
-                  this.setState({ monthsCount: value })
-                }
-                allowNegative={false}
-                className='form-control'
-              />
-            </FormGroup>
-          </Form>
+          <FormGroup>
+            <Label htmlFor='monthlyRent'>Monthly Rent</Label>
+            <NumberFormat
+              name='monthlyRent'
+              value={this.state.monthlyRent}
+              onValueChange={({ value }) => {
+                this.setState({ monthlyRent: parseFloat(value) })
+              }}
+              decimalScale={2}
+              thousandSeparator={true}
+              prefix={'$'}
+              allowNegative={false}
+              className='form-control'
+            />
+          </FormGroup>
           <hr />
-          <Row className='text-center'>
-            <Col xs='4' className='mb-3'>
-              <strong>Per Person Monthly Rent</strong>
-              <br />
-              {this.state.residentsCount >= 1
-                ? Dinero({
-                    amount: dollarsToCents(this.state.monthlyRent)
-                  })
-                    .divide(this.state.residentsCount)
-                    .toFormat()
-                : null}
-            </Col>
-            <Col xs='4' className='mb-3'>
-              <strong>Per Person Total Rent</strong>
-              <br />
-              {this.state.residentsCount >= 1
-                ? Dinero({
-                    amount: dollarsToCents(this.state.monthlyRent)
-                  })
-                    .divide(this.state.residentsCount)
-                    .multiply(this.state.monthsCount)
-                    .toFormat()
-                : null}{' '}
-            </Col>
-            <Col xs='4' className='mb-3'>
-              <strong>Overall Total Rent</strong>
-              <br />
-              {Dinero({
-                amount: dollarsToCents(this.state.monthlyRent)
-              })
-                .multiply(this.state.monthsCount)
-                .toFormat()}
-            </Col>
-          </Row>
+          <Summary
+            monthlyRent={this.state.monthlyRent}
+            tiers={this.state.tiers}
+          />
+          <hr />
+
+          <Card className='mb-3'>
+            <CardHeader>Tiers</CardHeader>
+            <ListGroup flush>
+              {this.state.tiers.map((tier, index) => (
+                <Tier
+                  key={index}
+                  update={this.updateTier.bind(this, index)}
+                  remove={this.removeTier.bind(this, index)}
+                  {...tier}
+                />
+              ))}
+            </ListGroup>
+            <CardFooter>
+              <Button block onClick={this.toggleModal} color='dark' block>
+                <i className='fas fa-plus'></i> Add Tier
+              </Button>
+            </CardFooter>
+          </Card>
+
+          <AddTierModal
+            isOpen={this.state.modal}
+            toggle={this.toggleModal}
+            add={this.addTier}
+          ></AddTierModal>
+
           <hr />
           <div className='text-center mb-3'>
             <small className='text-muted'>
@@ -122,6 +141,254 @@ class App extends Component {
           </div>
         </Container>
       </div>
+    )
+  }
+}
+
+class Tier extends Component {
+  state = {
+    modal: false
+  }
+
+  toggleModal = () => {
+    this.setState({ modal: !this.state.modal })
+  }
+
+  render () {
+    return (
+      <ListGroupItem>
+        <div className='d-flex justify-content-between align-items-center mt-2 mb-2'>
+          <h5 className='mb-0'>
+            <NumberFormat
+              value={this.props.monthlyRent}
+              displayType={'text'}
+              thousandSeparator={true}
+              prefix={'$'}
+            />
+          </h5>
+          <div className='d-flex justify-content-between align-items-center'>
+            <Badge className='mr-3'>
+              {this.props.residentsCount} Residents
+            </Badge>
+            <i onClick={this.toggleModal} className='fas fa-edit ml-1'></i>
+            <i
+              onClick={this.props.remove}
+              className='fas fa-trash-alt ml-1'
+            ></i>
+          </div>
+        </div>
+
+        <small>{this.props.description}</small>
+
+        <UpdateTierModal
+          isOpen={this.state.modal}
+          toggle={this.toggleModal}
+          update={this.props.update}
+          {...this.props}
+        ></UpdateTierModal>
+      </ListGroupItem>
+    )
+  }
+}
+
+class UpdateTierModal extends Component {
+  state = {
+    monthlyRent: this.props.monthlyRent,
+    description: this.props.description,
+    residentsCount: this.props.residentsCount
+  }
+
+  update = event => {
+    this.props.update({ ...this.state })
+    this.props.toggle()
+  }
+
+  render () {
+    return (
+      <Modal isOpen={this.props.isOpen} toggle={this.props.toggle} fade={false}>
+        <ModalHeader toggle={this.props.toggle}>Update Tier</ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup>
+              <Label htmlFor='monthlyRent'>Monthly Rent</Label>
+              <NumberFormat
+                name='monthlyRent'
+                value={this.state.monthlyRent}
+                onValueChange={({ value }) =>
+                  this.setState({ monthlyRent: parseFloat(value) })
+                }
+                decimalScale={2}
+                thousandSeparator={true}
+                prefix={'$'}
+                allowNegative={false}
+                className='form-control'
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor='residentsCount'>Number of Residents</Label>
+              <NumberFormat
+                name='residentsCount'
+                value={this.state.residentsCount}
+                onValueChange={({ value }) => {
+                  this.setState({ residentsCount: parseInt(value) })
+                }}
+                decimalScale={0}
+                allowNegative={false}
+                className='form-control'
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor='description'>Description</Label>
+              <Input
+                name='description'
+                type='textarea'
+                value={this.state.description}
+                onChange={event =>
+                  this.setState({ description: event.target.value })
+                }
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color='light' onClick={this.props.toggle}>
+            Cancel
+          </Button>
+          <Button color='dark' onClick={this.update}>
+            Update Tier
+          </Button>{' '}
+        </ModalFooter>
+      </Modal>
+    )
+  }
+}
+
+class AddTierModal extends Component {
+  initial = {
+    monthlyRent: 100,
+    description: '',
+    residentsCount: 0
+  }
+  state = { ...this.initial }
+
+  add = () => {
+    this.props.add(this.state)
+    this.props.toggle()
+    this.clear()
+  }
+
+  cancel = () => {
+    this.props.toggle()
+    this.clear()
+  }
+
+  clear = () => {
+    this.setState({ ...this.initial })
+  }
+
+  render () {
+    return (
+      <Modal isOpen={this.props.isOpen} toggle={this.cancel} fade={false}>
+        <ModalHeader toggle={this.cancel}>Add Tier</ModalHeader>
+        <ModalBody>
+          <Form onSubmit={this.add}>
+            <FormGroup>
+              <Label htmlFor='monthlyRent'>Monthly Rent</Label>
+              <NumberFormat
+                name='monthlyRent'
+                value={this.state.monthlyRent}
+                onValueChange={({ value }) =>
+                  this.setState({ monthlyRent: parseFloat(value) })
+                }
+                decimalScale={2}
+                thousandSeparator={true}
+                prefix={'$'}
+                allowNegative={false}
+                className='form-control'
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor='residentsCount'>Number of Residents</Label>
+              <NumberFormat
+                name='residentsCount'
+                value={this.state.residentsCount}
+                onValueChange={({ value }) =>
+                  this.setState({ residentsCount: parseInt(value) })
+                }
+                decimalScale={0}
+                allowNegative={false}
+                className='form-control'
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label htmlFor='description'>Description</Label>
+              <Input
+                name='description'
+                type='textarea'
+                value={this.state.description}
+                onChange={event =>
+                  this.setState({ description: event.target.value })
+                }
+              />
+            </FormGroup>
+          </Form>
+        </ModalBody>
+        <ModalFooter>
+          <Button color='light' onClick={this.cancel}>
+            Cancel
+          </Button>
+          <Button color='dark' onClick={this.add}>
+            Add Tier
+          </Button>{' '}
+        </ModalFooter>
+      </Modal>
+    )
+  }
+}
+
+class Summary extends Component {
+  render () {
+    const residentsCount = this.props.tiers.reduce(
+      (sum, tier) => sum + tier.residentsCount,
+      0
+    )
+    const monthlyRent = Dinero({
+      amount: dollarsToCents(this.props.monthlyRent)
+    })
+    const monthlyIncome = this.props.tiers.reduce(
+      (sum, tier) =>
+        sum.add(
+          Dinero({ amount: dollarsToCents(tier.monthlyRent) }).multiply(
+            tier.residentsCount
+          )
+        ),
+      Dinero()
+    )
+    const monthlyDelta = monthlyIncome.subtract(monthlyRent)
+
+    return (
+      <Row className='text-center'>
+        <Col xs={4} className='mb-3'>
+          <strong>Monthly Income</strong>
+          <br />
+          {monthlyIncome.toFormat()}
+        </Col>
+        <Col
+          xs={4}
+          className={`mb-3 ${
+            monthlyDelta.isNegative() ? 'text-danger' : 'text-success'
+          }`}
+        >
+          <strong>Monthly Delta</strong>
+          <br />
+          {monthlyDelta.toFormat()}
+        </Col>
+        <Col xs={4}>
+          <strong>Number of Residents</strong>
+          <br />
+          {residentsCount}
+        </Col>
+      </Row>
     )
   }
 }
