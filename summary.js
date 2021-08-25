b03b1a4ed1cd26f13c63a4d6c3fe90eb9ca4c868
Joshua Taylor Eppinette <jteppinette@jteppinette.com>
2021-08-25 12:43:05 -0600

    summary: Remove unused import

--- summary.js
+++ summary.js
@@ -1,5 +1,5 @@
 import Dinero from 'dinero.js'
-import React, { Component } from 'react'
+import React from 'react'
 import { Row, Col } from 'reactstrap'
 
 import { dollarsToCents } from './utils.js'
